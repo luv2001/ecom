0539f5094e9f92933cfaf7a91dd9d3a9aef85ba3
Luvdipsingh Dadiyala <ldadiyala@gmail.com>
2021-11-17 22:59:31 +0530

    build

---
+++ project_e_commerce-main/build/precache-manifest.4951545302ef6f3f6bbdc249d9b48422.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "033a29d3f39eb9acc63ee8b8c65682f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7033bc475038bd22f0fa",
+    "url": "/static/js/2.5dd4e996.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/static/js/2.5dd4e996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a500d8be91514b307c62",
+    "url": "/static/js/main.14aebf5a.chunk.js"
+  },
+  {
+    "revision": "ed8ed874e871633992a1",
+    "url": "/static/js/runtime-main.a418149b.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/static/media/commerce.422fec10.png"
+  }
+]);
